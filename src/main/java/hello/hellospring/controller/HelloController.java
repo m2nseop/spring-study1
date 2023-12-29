f82f3f4addd5571d3ca2114fb7524d3b4a342339
m2nseop <pms246@naver.com>
2023-12-29 19:22:28 +0900

    스프링 웹 통신 3가지 방법

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // 컨트롤러임을 명시하는 어노테이션
 @Controller
@@ -23,4 +24,35 @@ public class HelloController {
         return "hello-template";
     }
 
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    // 해당 어노테이션을 붙여주어야 spring은 api를 처리하는 컨트롤러임을 인지한다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; // "hello 파라미터값"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        // Spring은 @ResponseBody 어노테이션을 보고 api요청임을 감지
+        // 리턴되는 데이터가 객체이므로 HttepMessageConverter중 JsonConverter(디폴트는 MappingJackson2HttpMessageConverter)를 이용해 객체를 json으로 변환하여
+        // http 응답 body에 싣고 웹브라우저에 반환
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
